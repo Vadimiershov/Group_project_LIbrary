3a2b8cf7e498f0d2152c3e495323a88ce2877bfd
gstorohuk <galyna.storozhuk@gmil.com>
2021-02-25 00:23:25 -0500

    Completing US4 with 1st scenario completed

--- src/test/java/com/cybertek/library/step_definitions/US4_AddingUserLibrarian_StepDefinitions.java
+++ src/test/java/com/cybertek/library/step_definitions/US4_AddingUserLibrarian_StepDefinitions.java
@@ -5,16 +5,28 @@ import com.cybertek.library.pages.User_Page_Librarian;
 import com.cybertek.library.pages.UsersModule;
 import com.cybertek.library.utilities.BrowserUtils;
 import com.cybertek.library.utilities.Driver;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class US4_AddingUserLibrarian_StepDefinitions {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.cybertek.library.utilities.Memory.getMemory;
+
+public class US4_AddingUserLibrarian_StepDefinitions extends BrowserUtils {
 
 
     LoginPage homePage = new LoginPage();
@@ -23,7 +35,7 @@ public class US4_AddingUserLibrarian_StepDefinitions {
 
     @Given("user is on the homePage")
     public void user_is_on_the_home_page() {
-      homePage.homePageLanding();
+        homePage.homePageLanding();
 
     }
 
@@ -52,10 +64,11 @@ public class US4_AddingUserLibrarian_StepDefinitions {
     }
 
     @And("librarian can input new user's name {string}")
-    public void librarian_can_input_new_user_s(String userName) {
+    public void librarian_can_input_new_user_s(String key) {
         librarianPage.inputUserName.click();
-        librarianPage.inputUserName.sendKeys(userName);
+        librarianPage.inputUserName.sendKeys(getMemory().retrieveValue(key));
     }
+
 
     @And("librarian can input new user's password {string}")
     public void librarianCanInputNewUserSPassword(String password) {
@@ -63,10 +76,20 @@ public class US4_AddingUserLibrarian_StepDefinitions {
         librarianPage.inputPassword.sendKeys(password);
     }
 
+    @And("I generate new full name and email address and save it as {string} and {string}")
+    public void generateEmail(String name, String email) {
+        List<String> list = generateUserInfo();
+        String fullName = list.get(0);
+        String emailValue = list.get(1);
+        getMemory().storeValue(email, emailValue);
+        getMemory().storeValue(name, fullName);
+    }
+
+
     @And("librarian can input new user's email {string}")
     public void librarianCanInputNewUserSEmail(String email) {
         librarianPage.inputEmail.click();
-        librarianPage.inputEmail.sendKeys(email);
+        librarianPage.inputEmail.sendKeys(getMemory().retrieveValue(email));
 
     }
 
@@ -121,34 +144,46 @@ public class US4_AddingUserLibrarian_StepDefinitions {
         librarianPage.address.sendKeys(address);
 
     }
+
     @When("librarian should be able to click on Save Changes")
     public void librarian_should_be_able_to_click_on_save_changes() {
-        BrowserUtils.waitForVisibility(librarianPage.saveChanges,5);
+        BrowserUtils.waitForVisibility(librarianPage.saveChanges, 5);
         librarianPage.saveChanges.click();
 
     }
 
     @Then("new user {string} is successfully created")
-    public void newUserIsSuccessfullyCreated(String newUser) {
+    public void newUserIsSuccessfullyCreated(String key) {
+        //    String newUserActual = usersModule.userTableEmails.toString();
 
-        String newUserActual=usersModule.firstNameInTheColumn.getText();
+        //  String expectedEmail = getMemory().retrieveValue(newUser);
 
+//        Assert.assertEquals(expectedEmail, newUserActual);
+        BrowserUtils.wait(10);
+        Assert.assertTrue(librarianPage.confirmUser(key));
 
-
-            
-        }
-
-
-
+    }
 
 
     @When("librarian is on the add user window and clicks Close Button")
     public void librarian_is_on_the_add_user_window_and_clicks_close_button() {
+//        BrowserUtils.waitForClickability(librarianPage.usersButton, 5);
+//        librarianPage.usersButton.click();
+//        String expected = "User Management";
+//        String actual = librarianPage.userManagement.getText();
+//        Assert.assertTrue(actual.contains(expected));
+//
+//         Clicks close button
+
+//
+
 
     }
 
     @Then("librarian should be able close Add User input window")
     public void librarian_should_be_able_close_add_user_input_window() {
+
+        // Assert that user is back
 
     }
 
@@ -161,10 +196,27 @@ public class US4_AddingUserLibrarian_StepDefinitions {
     public void librarian_is_able_to_see_edit_user_information_window() {
 
     }
+
     @Then("librarian can edit any user info and save changes")
     public void librarian_can_edit_any_user_info_and_save_changes() {
 
     }
 
+
+    public List<String> generateUserInfo() {
+        List<String> list = new ArrayList<>(2);
+        Faker faker = new Faker();
+
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String fullName = firstName + " " + lastName;
+        String email = firstName + "." + lastName + "@mailnator.com";
+        list.add(fullName);
+        list.add(email);
+
+        return list;
+    }
+
 }
+
 
--- src/test/resources/features/US4_Add_User_Librarian.feature
+++ src/test/resources/features/US4_Add_User_Librarian.feature
@@ -1,7 +1,7 @@
 Feature: Librarian should be able to add users with valid info
 
   Background: User should land on a homepage
-  //  Given user is on the homePage
+    Given user is on the homePage
     Given user is successfully logged in
 	#AC:
 	#1. Add users with all valid info.
@@ -13,21 +13,22 @@ Feature: Librarian should be able to add users with valid info
   Scenario Outline:
     When librarian clicks on Add User button
     When librarian should see Add User prompt window
-    And librarian can input new user's name "<Full name>"
+    And I generate new full name and email address and save it as "fullName" and "email"
+    And librarian can input new user's name "fullName"
     And librarian can input new user's password "<Password>"
-    And librarian can input new user's email "<Email>"
+    And librarian can input new user's email "email"
     And librarian can input new user's user group"<User Group>"
     And librarian can input new user's status "<Status>"
     And librarian can input new user's start date"<Start Date>"
     And librarian can input new user's end date "<End Date>"
     And librarian can input new user's address "<Address>"
     And librarian should be able to click on Save Changes
-    Then new user "<Expected user>" is successfully created
+    Then new user "fullName" is successfully created
 
     Examples:
-      | Full name        | Password | Email                   | User Group | Status   | Start Date | End Date   | Address         | Expected user           |
-      | Beyonce B        | 123@456# | beyoncef@mail.ru        | Students   | ACTIVE   | 2021/03/01 | 2022/03/01 | San Diego, Ca   | beyoncef@mail.ru        |
-      | Shakira Gonzalez | 567@89&$ | shakirashakiraf@mail.ru | Librarian  | INACTIVE | 2022/02/01 | 2023/02/01 | Los Angeles, Ca | shakirashakiraf@mail.ru |
+      | Password | User Group | Status   | Start Date | End Date   | Address         | emailAddress |
+      | 123@456# | Students   | ACTIVE   | 2021/03/01 | 2022/03/01 | San Diego, Ca   | email        |
+      | 567@89&$ | Librarian  | INACTIVE | 2022/02/01 | 2023/02/01 | Los Angeles, Ca | email        |
 
 
   Scenario: librarian should be able to close the Add User window with Close Button
